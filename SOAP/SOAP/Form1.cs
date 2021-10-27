72408b0039e313a80424c37d456e21e1646af950
Márk Félix Mészáros <mark.meszaros@stud.uni-corvinus.hu>
2021-10-27 19:14:04 +0200

    3rd task commit

--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SOAP.MnbServiceReference;
 
 namespace SOAP
 {
@@ -15,6 +16,27 @@ namespace SOAP
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+
         }
     }
 }
