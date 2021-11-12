40580d9f380ebbe52664e3d0838f0034d2c0e928
Márk Félix Mészáros <mark.meszaros@stud.uni-corvinus.hu>
2021-11-12 17:12:54 +0100

    second task commit

---
+++ Futoszalag_8.het/Futoszalag_8.het/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futoszalag_8.het.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew() 
+        {
+            return new Ball();
+
+        }
+    }
+}
